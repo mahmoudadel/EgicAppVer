196321927ad550170008b73c5c4a32b9defc3364
Mahmoud.Adel <eng.mahmoudadel88@gmail.com>
2019-02-21 14:31:32 +0200

    live

--- recordversion/src/main/java/egic/com/recordversion/ApiClient.java
+++ recordversion/src/main/java/egic/com/recordversion/ApiClient.java
@@ -10,8 +10,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class ApiClient {
-    //public static final String BASE_URL = "http://Endahly.egic.com.eg:8086/AndroidAppsVersionsBE/api/";
-    public static final String BASE_URL = "http://10.110.10.132/AndroidAppsVersionsBE/api/";
+    public static final String BASE_URL = "http://Endahly.egic.com.eg:8086/AndroidAppsVersionsBE/api/";
+   // public static final String BASE_URL = "http://10.110.10.132/AndroidAppsVersionsBE/api/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
